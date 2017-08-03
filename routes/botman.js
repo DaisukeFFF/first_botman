be3fa5e4a9400a7a7a233347d63845de920a6417
Fujii daisuke <ifc.dai.faculty@gmail.com>
2017-08-04 01:58:32 +0900

    change

--- routes/botman.js
+++ routes/botman.js
@@ -50,28 +50,30 @@ bot.on('message', (event) => {
     if(event.message.type !== 'text'){
         return;
     }
-    if(event.message.text.indexOf('ほめて') !== -1){
-        event.source.profile().then((lineProfile) =>{
-            console.log(lineProfile);
-            const num = getRandom(1,3);
-            switch (num){
-                case 1:
-                    event.reply('素晴らしい！君は日本の宝だ！');
-                case 2:
-                    event.reply(lineProfile.displayName + 'さん！\nさすがっすね！');
-                case 3:
-                    event.reply('よっ！若頭！');
-                case 4:
-                    event.reply(lineProfile.displayName +'さん\n今日もお疲れ様！夜はゆっくり休んでくださいね');
-            }
-        }).catch((err) => {
+    event.source.profile().then((lineProfile) =>{
+        if(event.message.text.indexOf('ほめて') !== -1){
+                console.log(lineProfile);
+                const num = getRandom(1,3);
+                switch (num){
+                    case 1:
+                        event.reply('素晴らしい！君は日本の宝だ！');
+                    case 2:
+                        event.reply(lineProfile.displayName + 'さん！\nさすがっすね！');
+                    case 3:
+                        event.reply('よっ！若頭！');
+                    case 4:
+                        event.reply(lineProfile.displayName +'さん\n今日もお疲れ様！夜はゆっくり休んでくださいね');
+                }
+        }else if(event.message.text === 'ヘルプ'){
+            event.reply('お呼びですか?\n「ほめて」と言われたら褒めます。\n'+
+                         '今はただの褒め上手ですが、そのうち色々覚えていきますよ！');
+        }else if(event.message.text === 'くっころ'){
+            event.reply(lineProfile.displayName + 'にこんな辱めを受けるとは...！\nくっ...殺せ！');
+        }
+    }).catch((err) => {
          throw new Error(err);
-        });
-
-    }else if(event.message.text === 'ヘルプ'){
-        event.reply('お呼びですか?\n「ほめて」と言われたら褒めます。\n'+
-                     '今はただの褒め上手ですが、そのうち色々覚えていきますよ！');
-    }
+    });
+    
 });
 
 // router.post('/', function(req, res) {
