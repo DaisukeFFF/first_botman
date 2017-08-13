4a6935359418b6659a9ad96efa4fa50a6fc20fe9
Fujii daisuke <ifc.dai.faculty@gmail.com>
2017-08-14 02:52:22 +0900

    天気情報の追加

--- routes/botman.js
+++ routes/botman.js
@@ -84,8 +84,15 @@ const returnMessage = (event) =>{
             event.reply(message);
         }else if(event.message.text === '天気'){
             weatherData().then((tokyoWeather) => {
-                event.reply('本日の天気(東京)\n'+ '天候：'+ tokyoWeather.weather[0].main+ 
-                    '\n気温：'+ tokyoWeather.main.temp+ '℃');
+            message = {
+                type: 'image',
+                originalContentUrl: 'http://openweathermap.org/img/w/'+tokyoWeather.weather[0].icon +'.png',
+                previewImageUrl: 'http://openweathermap.org/img/w/'+tokyoWeather.weather[0].icon +'.png'
+            }
+                event.reply(['本日の天気(東京)\n'+ '天候：'+ tokyoWeather.weather[0].main+ 
+                    '\n気温：'+ tokyoWeather.main.temp+ '℃',
+                    message
+                    ]);
             });
         }else if(event.message.text === 'お天気'){
             message = {
